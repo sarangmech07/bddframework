d17b58875c192e482c3414c90b3ec4ffcfbec407
Saranga Bikash Baruah <sarbarua@publicisgroupe.net>
2021-01-19 21:32:20 +0530

    Added bdd framework poc

---
+++ test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "1862c7c7-0d9c-44a4-a67e-32365c822e0c",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1610649811413,
+    "group": 15,
+    "content": "",
+    "tags": "@smoke,",
+    "end": 1610649821279,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 15,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  }
+]);
+});
